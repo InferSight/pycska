9cf023d0dd920edf066896eeb40be689530540c1
Steve Graham <sgraham@infersight.com>
2017-09-18 22:46:07 +0000

    Added setup file for installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pycska',
+      version='1.0',
+      description='Infersight CSKA API',
+      author='Steve Graham',
+      author_email='sgraham@infersight.com',
+      url='http://www.infersight.com',
+      packages=['pycska']
+     )
